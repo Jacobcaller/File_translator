76e2cfc88e727ae366342c6aab850b8c74ee47a6
Jacobcaller <hovorkaobch@gmail.com>
2021-12-04 00:16:01 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+from deep_translator import GoogleTranslator #Imported google translator from deep_translator module.
+from os import getcwd #Module that gets current path of main.py.
+
+print("Welcome in my translator. Insert name of the file like this: example.txt")
+while True: #While loop for repeatability
+    lang_list = GoogleTranslator.get_supported_languages() #This variable holds all available languages.
+    name = input("Insert name of the file: ") #Name of user's file for translation.
+    path = getcwd() + "\\" + name #Path for opening user's file.
+    try: #Try/except to take care of errors caused by non-existent file.
+        with open(path, "r") as tr_file: #Opening user's file for reading to get lines for translation.
+            try: #Try/except to take care of errors caused by choosing non-existing language, typos etc.
+                with open("translated_" + name, "w") as translated: #Creating new file to save translated lines.
+                    print("Languages you can choose to translate to: \n",",".join(lang_list)) #Printing languages available for translation.
+                    tr_to = input("Choose language that you want to translate your file to: ") #User's choice of language for translation.
+                    progress = 0 #Variable for showing listing line that's currently being translated.
+                    for line in tr_file.readlines(): #For loop that iterates over all lines in user's file.
+                        progress += 1  # Adding 1 for every line that's been translated.
+                        translated.write(GoogleTranslator(source="auto", target=tr_to).translate(line) + "\n") #Writing translated lines into new file.
+                        print(f"Translating line {progress}") #Showing line that's currently being translated.
+                print(f"All {progress} lines were translated successfully.") #Translation completed message.
+            except Exception as err: #Takes care of error caused when choosing language for translation.
+                print("Something went wrong.")
+                print(err) #Prints error message.
+    except FileNotFoundError: #Takes care of error caused by wrong file name.
+        print(f"File {path} does not exist.")
+    finally: #Asks if user wants to end the program or continue to translation of another file.
+        if input("Do you want to continue?(Y/N): ") == "N":
+            break
